45238af761e4bab5ee9ea664a805895c1f25d527
Yernemm <yernemm@yernemm.xyz>
2019-12-02 23:58:37 +0000

    basic stuff

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
+const Discord = require("discord.js");
 class SnoCord
 {
 
+    /**
+     * Creates a new Discord Client.
+     */
+    constructor()
+    {
+        this._client = new Discord.Client();
+
+        this._client.on('ready', () => {
+            console.log(`Logged in as ${client.user.tag}.\nRunning SnoCord.`);
+          });
+          
+        this._client.on('message', msg => {
+            if (msg.content === 'ping') {
+              msg.reply('Pong!');
+            }
+          });
+
+        this._responses = [];
+    }
+
+    /**
+     * Logs into the Discord client
+     * @param {string} token - The Discord token to log in with.
+     * @returns {Promise<string>} - Token of the account used.
+     */
+    logIn(token)
+    {
+        return this._client.login(token);
+    }
+
+    /**
+     * Discord.js Client.
+     */
+    get client()
+    {
+        return this._client;
+    }
+
+    addResponse(trigger, guilds = [], channels = [], reply)
+    {
+        
+    }
 }
 module.exports = SnoCord;
