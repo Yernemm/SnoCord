b5a29622473f733c6aa79de83dfcc6056bbe7cc3
Bryson <ingoldbw281@gmail.com>
2019-12-03 19:24:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,98 @@
-const Discord = require("discord.js");
-class SnoCord
+'use strict';
+const Discord = require('discord.js');
+const EventEmitter = require('events');
+
+// A set containing all of Discord.js' events except `message`
+const events = new Set(Object.values(Discord.Constants.Events));
+events.delete('message');
+
+class SnoCord extends EventEmitter
 {
-
     /**
-     * Creates a new Discord Client.
+     * Creates a new SnoCord instance.
      */
-    constructor()
+    constructor(opts)
     {
-        this._client = new Discord.Client();
+        super();
+        const options = mergeDefault(opts, SnoCord.defaultOptions);
 
-        this._client.on('ready', () => {
-            console.log(`Logged in as ${client.user.tag}.\nRunning SnoCord.`);
-          });
-          
-        this._client.on('message', msg => {
-            if (msg.content === 'ping') {
-              msg.reply('Pong!');
-            }
-          });
+        if (!options.config) throw new Error('No config object provided');
+        this.config = options.config;
+        this.client = new Discord.Client(options.client);
 
-        this._responses = [];
+        this.commands = new Map();
+        this.invokers = new Set();
     }
 
     /**
-     * Logs into the Discord client
+     * Intializes the SnoCord instance.
      * @param {string} token - The Discord token to log in with.
-     * @returns {Promise<string>} - Token of the account used.
+     * @param {Object} [callback] - An object defining functions to call before or after the bot connects to the discord API
+     * @returns {Promise<void>}
+     * @example
+     * new SnoCord(options).init({
+     *   async preInit() {
+     *     // Do stuff before connecting to the API
+     *   },
+     *   async postInit() {
+     *     // Do stuff after connecting to the API
+     *   }
+     * });
      */
-    logIn(token)
+    async init(callbacks)
     {
-        return this._client.login(token);
-    }
+        const { preInit, postInit } = mergeDefault(callbacks, SnoCord.defaultInitCallbacks);
 
-    /**
-     * Discord.js Client.
-     */
-    get client()
-    {
-        return this._client;
-    }
+        if (!this.config.token) throw new Error('No token provided');
 
-    addResponse(trigger, guilds = [], channels = [], reply)
-    {
-        
+        if (preInit) await preInit(this);
+
+        await this.client.login(this.config.token);
+
+        this.client.once('ready', async () => {
+            if (postInit) await postInit(this);
+
+            for (let event of events) {
+                this.client.on(event, (...args) => {
+                    this.emit(event, ...args);
+                });
+            }
+
+            this.client.on('message', (message) => {
+                if (messageIsCommand) {
+                    // Message is a command, execute command somehow
+                } else if (messageIsInvoker) {
+                    // Message is an invoker, execute invoker somehow
+                } else {
+                    // Message is a normal message
+                    this.emit('message', message);
+                }
+            });
+        });
     }
 }
+
+SnoCord.defaultOptions = {
+    config: null,
+    client: {}
+};
+
+SnoCord.defaultInitCallbacks = {
+    preInit: null,
+    postInit: null
+};
+
 module.exports = SnoCord;
+
+function mergeDefault(def, given) {
+    if (!given) return def;
+    for (const key in def) {
+        if (!{}.hasOwnProperty.call(given, key)) {
+            given[key] = def[key];
+        } else if (given[key] === Object(given[key])) {
+            given[key] = mergeDefault(def[key], given[key]);
+        }
+    }
+  
+    return given;
+}
